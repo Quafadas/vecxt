279a9524cd17bf83d5aaca99b742552a7aefbeef
partens <simon.parten@schroders.com>
2025-02-12 16:42:06 +0100

    .

--- vecxt/jvm-native/src/doublematrix.scala
+++ vecxt/jvm-native/src/doublematrix.scala
@@ -4,14 +4,14 @@ import vecxt.arrays.*
 import vecxt.matrix.*
 
 object JvmNativeDoubleMatrix:
-  extension (m: Matrix[Double])
+  // extension (m: Matrix[Double])
 
-    inline def +(m2: Matrix[Double])(using inline boundsCheck: BoundsCheck): Matrix[Double] =
-      sameDimMatCheck(m, m2)
-      val newArr = m.raw.add(m2.raw)
-      Matrix(newArr, m.shape)(using BoundsCheck.DoBoundsCheck.no)
-    end +
-  end extension
+  //   inline def +(m2: Matrix[Double])(using inline boundsCheck: BoundsCheck): Matrix[Double] =
+  //     sameDimMatCheck(m, m2)
+  //     val newArr = m.raw.add(m2.raw)
+  //     Matrix(newArr, m.shape)(using BoundsCheck.DoBoundsCheck.no)
+  //   end +
+  // end extension
 end JvmNativeDoubleMatrix
 
 object JsDoubleMatrix:
