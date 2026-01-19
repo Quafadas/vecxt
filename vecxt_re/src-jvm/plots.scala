e76140f97b76015c61ae0cb99cc583f730999dce
Simon Parten <quafadas@gmail.com>
2026-01-19 22:44:04 +0100

    .

--- vecxt_re/src-jvm/plots.scala
+++ vecxt_re/src-jvm/plots.scala
@@ -5,4 +5,5 @@ import io.github.quafadas.plots.SetupVega.*
 object Plots:
   lazy val timeline = VegaPlot.fromResource("timeline.vl.json") // riskInceptionDate, riskExpiryDate
   lazy val seasonality = VegaPlot.fromResource("seasonality.vg.json") // catagory, amount
+  lazy val distributionDensity = VegaPlot.fromResource("distDensity.vg.json") // value, density
 end Plots
