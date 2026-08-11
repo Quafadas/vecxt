2281d0f27ede8e2fd6b2b5dd8ad1cee04555c697
Simon Parten <Quafadas@users.noreply.github.com>
2026-08-11 14:59:45 +0000

    .

--- vecxt/test/src-jvm/TODO.test.scala
+++ vecxt/test/src-jvm/TODO.test.scala
@@ -24,9 +24,7 @@ class TODO extends FunSuite:
       0
     )
     val out = Matrix.zeros[Float]((2, 2))
-
-    // `matmulInPlace!`'s final `else` used to be a bare `???` (NotImplementedError); it now throws a named
-    // UnsupportedLayoutException carrying both operands' layouts, so this is no longer a TODO.
+    
     intercept[UnsupportedLayoutException] {
       left.`matmulInPlace!`(right, out, alpha = 1.0f, beta = 0.0f)
     }
